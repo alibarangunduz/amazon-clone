d31d059082d0773c363a76e2f7ce91c2f3248fa8
alibaran <alibarangunduzz@gmail.com>
2020-09-14 20:03:58 +0300

    update login

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState("");
+  
+  const signIn = e => {
+    e.preventDefault();
+    //some firebase login
+  }
+  const register = (e) => {
+    e.preventDefault();
+    // do some firebase register
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -16,11 +29,11 @@ function Login() {
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
           <h5>Password</h5>
-          <input type="password" />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
-          <button className="login__signInButton">Sign In</button>
+          <button type="submit" className="login__signInButton" onClick={signIn}>Sign In</button>
         </form>
 
         <p>
@@ -28,7 +41,7 @@ function Login() {
           Sale. Please see our Privacy Notice, our Cookies Notice and out
           Interest-Based Ads Notice!asdfsdffsdf
         </p>
-        <button className="login__registerButton">
+        <button className="login__registerButton" onClick={register}>
           Create your Amazon Account
         </button>
       </div>
