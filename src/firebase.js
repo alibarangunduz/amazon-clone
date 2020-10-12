8f9e71182a2ce43286087ef8a43063d768d319cc
Ali Baran Gündüz <58346472+alibarangunduz@users.noreply.github.com>
2020-10-12 15:01:29 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,7 @@
 import firebase from "firebase";
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAvCcYemHRiwi_7gz0C5plHbiU6BZozL0g",
-  authDomain: "clone-bb3a3.firebaseapp.com",
-  databaseURL: "https://clone-bb3a3.firebaseio.com",
-  projectId: "clone-bb3a3",
-  storageBucket: "clone-bb3a3.appspot.com",
-  messagingSenderId: "302664935542",
-  appId: "1:302664935542:web:899b17a1a587cadf8f5757",
-  measurementId: "G-G2WQHXE8Y3",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
